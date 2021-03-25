1707f343197778b90f9de10baff0916e00c80abb
Vinay Pai <vinay@vinaypai.com>
2021-03-25 13:33:29 -0400

    Add example of limiting selected date range

--- docs/app/pages/Components/Datepicker/examples/DaterangePicker.vue
+++ docs/app/pages/Components/Datepicker/examples/DaterangePicker.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <md-daterange-picker v-model="selectedDate" />
-  </div>
+    <div class="block">
+      <h3>Basic</h3>
+      <md-daterange-picker v-model="selectedRange" />
+    </div>
+    <div class="block">
+      <h3>Only Allow &plusmn; 7 Days of Today</h3>
+      <md-daterange-picker :md-disabled-dates="dateFilter" v-model="selectedLimitedRange" />
+    </div>  </div>
 </template>
 
 <script>
+  import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
+
   export default {
     name: 'DaterangePicker',
     data: () => ({
-      selectedDate: null
+      selectedRange: null,
+      selectedLimitedRange: null
     }),
+    methods: {
+      dateFilter(date) {
+        const today = new Date();
+        return Math.abs(differenceInCalendarDays(today, date)) > 7;
+      }
+    }
   }
 </script>
